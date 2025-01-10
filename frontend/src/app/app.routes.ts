bab4b25053611bce420932f279bff6c41b344acd
Yana Burdakova <bu.asti@gmail.com>
2025-01-10 10:14:07 -0500

    feat: add AuthComponent as start page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,5 +2,7 @@ import { Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth.component';
 
 export const routes: Routes = [
-  {path: '', component:AuthComponent}
+  {path: '', component: AuthComponent},
+
+
 ];
