dd48d0b4f38240e3e9a4642b62ae7d5445e2f15d
Yana Burdakova <bu.asti@gmail.com>
2025-01-10 11:52:38 -0500

    feat: add DashboardComponent

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  loggedInUser: { FirstName: string; LastName: string } | null = null;
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    const user = localStorage.getItem('loggedInUser');
+    if (user) {
+      this.loggedInUser = JSON.parse(user);
+    } else {
+      this.router.navigate(['/auth']);
+    }
+  }
+
+  logout(): void {
+    localStorage.removeItem('loggedInUser');
+    this.router.navigate(['/']);
+  }
 }
