a695f3c9c9d9e3e12cdfedd9b222f831a66edb9e
Yana Burdakova <bu.asti@gmail.com>
2025-01-09 19:53:21 -0500

    feat: add fonts to project styles

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './pages/auth/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component:AuthComponent}
+];
