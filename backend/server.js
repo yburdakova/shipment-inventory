84bcb778ed32174cd55505eb49b531403af96e76
CCS\yana.b <yana.b@courthousecomputersystems.com>
2025-03-07 12:26:48 -0500

    chore: add https to backend side

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import https from 'https';
+import fs from 'fs';
 
 import authRoutes from './routes/auth.routes.js';
 import projectRoutes from './routes/project.routes.js';
@@ -16,13 +18,20 @@ const dbname = process.env.DB_NAME;
 app.use(cors());
 app.use(express.json());
 
-
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/boxed', boxedRoutes);
 app.use('/api/returns', returnRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Local Server is running on port:${PORT}`);
+const keyPath = 'C:/SSL/server.key';
+const certPath = 'C:/SSL/server.cert';
+
+const sslOptions = {
+    key: fs.readFileSync(keyPath),
+    cert: fs.readFileSync(certPath)
+};
+
+https.createServer(sslOptions, app).listen(PORT, () => {
+    console.log(`Server is running on HTTPS: https://evieapi.courthousecomputersystems.com:${PORT}`);
     console.log(`Server is connecting to db ${dbname}`);
 });
